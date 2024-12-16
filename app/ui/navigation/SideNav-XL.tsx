b47b49d5642401cd40b6650b32dbccae3c32c2ad
Qntlz <uZyke9517@gmail.com>
2024-12-16 09:00:01 +0800

    Initial Commit Large Screen Sidenav

---
+++ app/ui/navigation/SideNav-XL.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link"
+import NavLinks from "./nav-links";
+import UsernameDisplay from "@/app/lib/actions";
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
+
+export default function SideNavXL() {
+    return (
+
+        <div className="hidden lg:flex flex-col ml-8 mt-2">
+            <span className="font-semibold text-2xl text-red-500">Dashboard</span>
+            <ul className="mt-4 mb-8">
+                <li>
+                    <Link href={'/home'}><UsernameDisplay /></Link>
+                </li>
+                <li>
+                    <Link href={'/home'}>BSCS - 2</Link>
+                </li>
+                <div className="hidden lg:flex p-2 mt-3 gap-2 text-red-500 border transition ease-in-out delay-150 border-red-500 rounded-xl
+                                hover:bg-red-500 hover:text-white items-center xl:justify-center">
+                    <div>
+                        <Link href={'/post/make'} className="flex font-normal text-sm text-nowrap items-center space-x-2">
+                            <PencilSquareIcon className="w-6" />
+                        </Link>
+                    </div>
+                    <div className="mt-1">
+                        <span className="hover:underline">New Post</span>
+                    </div>
+                </div>
+            </ul>
+            < NavLinks />
+        </div>
+    );
+}
