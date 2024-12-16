776524672092da92c62b7ca7826a16a3eae447a7
Qntlz <uZyke9517@gmail.com>
2024-12-16 15:44:39 +0800

    Initial Commit Chat

---
+++ app/(main_site)/chat/page.tsx
@@ -0,0 +1,9 @@
+import ChatApp from "@/app/ui/chat/chat";
+
+export default function ChatPage() {
+  return (
+    <div className="mt-20 ml-20">
+      <ChatApp />
+    </div>
+  );
+}
---
+++ app/ui/chat/message-input.tsx
@@ -0,0 +1,26 @@
+import { MessageInputProps } from "@/app/lib/definitions";
+
+export default function MessageInput({
+    newMessage,
+    setNewMessage,
+    sendMessage,
+}: MessageInputProps) {
+    return (
+        <div className="p-4 border-t border-gray-200 bg-gray-50 flex">
+            <input
+                type="text"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+                placeholder="Type a message..."
+                className="flex-1 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-300 transition"
+            />
+            <button
+                onClick={sendMessage}
+                className="ml-3 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition"
+            >
+                Send
+            </button>
+        </div>
+    );
+}
