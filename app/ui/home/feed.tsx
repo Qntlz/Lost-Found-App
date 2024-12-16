a024cbd97f04db55c0d688edc79e6b6d8cc471ff
Qntlz <uZyke9517@gmail.com>
2024-12-16 08:58:10 +0800

    Initial Commit Feed Component

---
+++ app/ui/home/feed.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { useState } from "react";
+import FeedHeader from "./feed-header";
+import Posts from "./feed-content";
+import { user, Post } from "@/app/lib/definitions";
+
+export default function Feed(){
+
+  const [items, setItems] = useState<Post[]>([]);
+  const [selectedStatus, setSelectedStatus] = useState("Missing");
+  const [selectedCampus, setSelectedCampus] = useState("All");
+  const [selectedBuilding, setSelectedBuilding] = useState("All");
+  const [userList, setUserList] = useState<user[]>([]);
+  const [showFilters, setShowFilters] = useState(false); // State to toggle filter visibility
+
+  return(
+    <div>
+      < FeedHeader showFilters={showFilters} selectedStatus={selectedStatus}
+        selectedCampus={selectedCampus} selectedBuilding={selectedBuilding} 
+        setSelectedStatus={setSelectedStatus} setSelectedCampus={setSelectedCampus} 
+        setSelectedBuilding={setSelectedBuilding} setShowFilters={setShowFilters}/>
+
+      <div className={`overflow-y-scroll no-scrollbar mt-5 ${showFilters ? "h-[60vh] mt-5" : "h-[70vh]"}`}>
+        < Posts userList={userList} selectedStatus={selectedStatus}
+          selectedCampus={selectedCampus} selectedBuilding={selectedBuilding} 
+          items={items} showFilters={showFilters} setItems={setItems} 
+          setUserList={setUserList} />
+      </div>
+
+    </div>
+  );
+}
