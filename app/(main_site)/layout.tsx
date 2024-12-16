58dcaf9ea31c5a648ad0f3e429433c5c61ef3a88
Qntlz <uZyke9517@gmail.com>
2024-12-16 17:07:46 +0800

    Major layout changes

--- app/(main_site)/layout.tsx
+++ app/(main_site)/layout.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { usePathname } from "next/navigation"; // Import the hook
-// import Header from "../ui/header/navbar";
-// import SideNavXL from "../ui/home/SidenavSimple";
+import NavBar from "../ui/navigation/navbar";
+import SideNavXL from "../ui/navigation/SideNav-XL";
 
 
 export default function RootLayout({
@@ -15,15 +15,26 @@ export default function RootLayout({
   // Define routes where the Header should be hidden
   const hideHeaderRoutes = ["/login", "/signup", "/test", "/"];
   const hideSideNavRoutes = ["/post/make"];
-
+  // lg:basis-1/5 2xl:basis-1/6
   return (
     <div>
-      <link rel="icon" href="/logo.svg" />
-      {/* Conditionally render Header only on routes not listed in hideHeaderRoutes */}
-      {/* {!hideHeaderRoutes.includes(pathname) && <Header />} */}
-        <div className="">
-          {children}
-        </div>
+  <link rel="icon" href="/logo.svg" />
+  {/* Conditionally render Header only on routes not listed in hideHeaderRoutes */}
+  {!hideHeaderRoutes.includes(pathname) && <NavBar />}
+  <div className="flex">
+    {/* Conditionally apply styles and render SideNavXL */}
+    {!hideHeaderRoutes.includes(pathname) ? (
+      <div className="lg:basis-1/5 2xl:basis-1/6">
+        <SideNavXL />
+      </div>
+    ) : (
+      <div></div> // If needed, leave empty or style as desired
+    )}
+    <div className="flex-grow">
+      {children}
     </div>
+  </div>
+</div>
+
   );
 }
